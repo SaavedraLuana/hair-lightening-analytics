d68972b19396224e398a7417de31ccc64a321724
Luana Saavedra <m7m4977dw8@privaterelay.appleid.com>
2026-08-08 18:32:56 +0200

    Add basic SQL queries for data retrieval
    Added basic SQL queries to view products, treatments, hair observations, and treatment progress.

---
+++ sql/01_basic_queries.sql
@@ -0,0 +1,19 @@
+-- View all products
+
+SELECT *
+FROM products;
+
+-- View all treatments
+
+SELECT *
+FROM treatments;
+
+-- View all hair observations
+
+SELECT *
+FROM hair_observations;
+
+-- View treatment progress
+
+SELECT *
+FROM progress;
